f882401314568aeb59f60a5557c9158075f2e566
L3ofspades <jonathanfederico13@gmail.com>
2025-04-24 20:08:24 -0700

    Create time-travel.js

---
+++ time-travel.js
@@ -0,0 +1,34 @@
+/* Task 1: Declare a Destination Variable */
+// TODO: Use `let` to declare a variable named `destination` and assign it the value `"Ancient Egypt"`. Print the destination to the console.
+let destination = "Ancient Egypt"
+console.log("intial Destination:", destination);
+/* Task 2: Change the Destination */
+// TODO: Now, change the `destination` variable to `"Medieval Europe"`. Print the new destination to the console.
+destination = "Medieval Europe";
+console.log("Updated Destination", destination);
+
+
+/* Task 3: Declare a Constant Travel Date */
+// TODO: Use `const` to declare a variable named `travelDate` and set it to `"2024-03-15"`. Try to change the `travelDate` to another value and observe and explain what happens as a comment.
+const travelDate = "2024-03-15";
+console.log("Travel Date:". travelDate);
+
+/*
+ * Observations:
+ * TODO: Explain here.
+ *
+ * // let allows us to decale a variable and later change its value
+ * const you cannot change 
+ * 
+
+/* Task 4: Experiment with Variable Hoisting */
+// TODO: Before declaring any variable, try to print a variable named `timeMachineModel` to the console. Then, declare `timeMachineModel` using `var` and assign it the value `"T-800"`. Observe and explain what happens as a comment.
+console.log("Time Machine Model before declaration:", timeMachineMode);
+
+var timeMachineModel = "T-800"
+comnsole.log("Time Machine Model after declaration:", timeMachineModel);
+/*
+ * Observations:
+// Variables declared with var are hoisted to the top of their scope
+ * TODO: Explain here.
+ */
